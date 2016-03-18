9ac13c8c195b72218579b0d8521c8a0b49c87ab4
GabrielRF <github@gabrf.com>
2016-03-18 14:11:55 -0300

    Fixed $YOURLS_URL_CONVERT

--- init.sh
+++ init.sh
@@ -153,7 +153,7 @@ define( 'YOURLS_PRIVATE', true );
         '$YOURL_USER' => '$YOURL_PASSWORD',
         );
 define( 'YOURLS_DEBUG', false );
-define( 'YOURLS_URL_CONVERT', 36 );
+define( 'YOURLS_URL_CONVERT', '$YOURLS_URL_CONVERT' );
 \$yourls_reserved_URL = array(
         'reserved',
 );
@@ -178,7 +178,7 @@ define( 'YOURLS_PRIVATE', true );
         '$YOURL_USER' => '$YOURL_PASSWORD',
         );
 define( 'YOURLS_DEBUG', false );
-define( 'YOURLS_URL_CONVERT', 36 );
+define( 'YOURLS_URL_CONVERT', '$YOURLS_URL_CONVERT'  );
 \$yourls_reserved_URL = array(
         'reserved',
 );
